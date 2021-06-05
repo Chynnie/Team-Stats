d3723289d0afe7ff6f4cf3cb324256d26dfe9ed2
Chynnie <33658059+Chynnie@users.noreply.github.com>
2021-06-05 15:30:12 +0100

    Create teamStats.js

---
+++ teamStats.js
@@ -0,0 +1,49 @@
+const team = {
+  _players: [
+    {
+      firstName: 'Pablo',
+      lastName: 'Sanchez',
+      age: 11
+    }
+  ],
+  _games: [
+    {
+      opponent: 'Broncos',
+      teamPoints: 42,
+      opponentPoints: 27
+    }
+  ],
+
+  get players() {
+    return this._players;
+  },
+  get games() {
+    return this._games;
+  },
+
+  addPlayer(firstName, lastName, age) {
+    let player = {
+      firstName: firstName,
+      lastName: lastName,
+      age: age
+    };
+    this.players.push(player);
+  },
+
+  addGame(oppName, teamPts, oppPts) {
+    let game = {
+      opponent: oppName,
+      teamPoints: teamPts,
+      opponentPoints: oppPts
+    };
+    this.games.push(game);
+  }
+};
+
+team.addPlayer('Steph', 'Curry', 28);
+team.addPlayer('Lisa', 'Leslie', 44);
+team.addPlayer('Bugs', 'Bunny', 76);
+console.log(team.players);
+
+team.addGame('Ravens', 50, 10);
+console.log(team.games);
